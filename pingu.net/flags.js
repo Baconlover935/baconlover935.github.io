687b64e7977641f352f2bdbee0b8e05234d8e589
Baconlover935 <julesmg1210@gmail.com>
2025-12-08 16:56:43 -0500

    Update flags.js

--- pingu.net/flags.js
+++ pingu.net/flags.js
@@ -3,7 +3,7 @@ country = new Array("UK", "USA", "Deutschland", "Schwiez", "France");
 url = new Array("uk", "us", "de", "ch", "fr");
 output = '<table cellpadding="4" cellspacing="0"><tr>';
 for ( i = 0; i < flag.length; i++ ) {
-	output += '<td><a href="https://baconlover935.github.io/pingu.net/' + url[i] + '"><img src="https://web.archive.org/web/20131217191845/http://www.hitentertainment.com/flags/flag' + flag[i] + '.gif" border="0" alt="'+country[i]+'" title="'+country[i]+'"/></a></td>';
+	output += '<td><a href="https://baconlover935.github.io/pingu.net/' + url[i] + '"><img src="https://baconlover935.github.io/flags/flag' + flag[i] + '.gif" border="0" alt="'+country[i]+'" title="'+country[i]+'"/></a></td>';
 }
 output += '	</tr></table>';
 document.write(output);
