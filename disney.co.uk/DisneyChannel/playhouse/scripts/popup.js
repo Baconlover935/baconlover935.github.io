545046fc1ca3813c1e3420bd5ff5e70caee19e6a
Baconlover935 <julesmg1210@gmail.com>
2026-05-12 16:26:09 -0400

    Create popup.js

---
+++ disney.co.uk/DisneyChannel/playhouse/scripts/popup.js
@@ -0,0 +1,7 @@
+function openWin(link,w,h,s) {
+//where 's' = scrollbars - 'yes' or 'no'
+dummy=window.open(link,"mywindow", "toolbar=0, status=no, menuBar=0, scrollbars, resizable=1, width="+w+", height="+h+", left=20, top=20");
+}
+
+
+}
